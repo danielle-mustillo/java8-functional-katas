d86963f3e00f5ee9e357059f43f42c537e2e16e5
Danielle Mustillo <danielle.mustillo@ericsson.com>
2018-02-16 10:19:39 -0500

    init commit, fixing for my environment and completing first kata

--- src/main/java/katas/Kata1.java
+++ src/main/java/katas/Kata1.java
@@ -7,6 +7,7 @@ import util.DataUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /*
     Goal: use map() to project an array of videos into an array of {id, title}-pairs
@@ -16,7 +17,9 @@ import java.util.Map;
 public class Kata1 {
     public static List<Map> execute() {
         List<Movie> movies = DataUtil.getMovies();
-
-        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
+        return movies.stream()
+                .map(movie -> ImmutableMap.of(movie.getId(), movie.getTitle()))
+                .collect(Collectors.toList());
+//        return ImmutableList.of(ImmutableMap.of("id", 5, "title", "Bad Boys"));
     }
 }
